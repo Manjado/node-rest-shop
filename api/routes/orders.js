ea8b2275f4646cb267bb3b196c1acdf29031d588
Manjado <michalalchimowicz@gmail.com>
2021-09-01 23:01:56 +0200

    model order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,21 +1,38 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")
+
+const Order = require("../models/order")
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Orders test",
-  })
+  Order.find()
+    .exec()
+    .then((docs) => {
+      res.status(200).json(docs)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
 })
 
 router.post("/", (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
     quantity: req.body.quantity,
-  }
-  res.status(201).json({
-    message: "Order was created",
-    order: order,
+    product: req.body.productId,
   })
+  console.log("ORDER:", order)
+  order
+    .save()
+    .then((result) => {
+      console.log(result)
+      res.status(201).json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: err })
+    })
 })
 
 router.get("/:orderId", (req, res, next) => {
