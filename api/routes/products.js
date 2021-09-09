20a5cc5b6687111012a007127d73d99190f083d0
Manjado <michalalchimowicz@gmail.com>
2021-09-09 20:56:35 +0200

    upload images

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,20 +1,49 @@
 const express = require("express")
 const router = express.Router()
 const mongoose = require("mongoose")
+const multer = require("multer")
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log("dest")
+    cb(null, "./uploads")
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toDateString() + file.originalname)
+  },
+})
+
+const fileFilter = (req, file, cb) => {
+  console.log("FILE:", file)
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    console.log("IF")
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
+}
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter,
+})
 
 const Product = require("../models/product")
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select("name price _id")
+    .select("name price _id productImage")
     .exec()
     .then((docs) => {
       const response = {
         count: docs.length,
-        products: docs.map(({ name, price, _id }) => {
+        products: docs.map(({ name, price, _id, productImage }) => {
           return {
             name,
             price,
+            productImage,
             _id,
             request: { type: "GET", url: "Http://localhost:3000/" + _id },
           }
@@ -28,11 +57,12 @@ router.get("/", (req, res, next) => {
     })
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", upload.single("productImage"), (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
+    productImage: req.file.path,
   })
   product
     .save()
@@ -56,7 +86,7 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId
   Product.findById(id)
-    .select("name price _id")
+    .select("name price _id productImage")
     .exec()
     .then((doc) => {
       if (doc) {
